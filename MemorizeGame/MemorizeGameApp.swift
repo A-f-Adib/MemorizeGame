7dc80903360a1d1c0c4a04165366ab506f059a05
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-10-22 02:21:04 +0600

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by A.f. Adib on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
